83969888e6652f9dc7f2370d8e31f82fc49febce
Nicola Soranzo <nicola.soranzo@earlham.ac.uk>
2022-04-30 02:48:02 +0100

    Build pure python wheels with `build` package

--- wheel_builder.py
+++ wheel_builder.py
@@ -2,6 +2,9 @@
 import os
 import subprocess
 import sys
+import tarfile
+import tempfile
+from pathlib import Path
 
 import yaml
 import requests
@@ -25,6 +28,7 @@ if __name__ == "__main__":
         meta = yaml.safe_load(f)
     package_name = meta["name"]
     package_version = meta["version"]
+    is_package_pure = meta.get("purepy", False)
 
     # Find the sdist url using the PyPI warehouse API https://warehouse.pypa.io/api-reference/json.html
     pypi_url = f"https://pypi.org/pypi/{package_name}/{package_version}/json"
@@ -36,29 +40,40 @@ if __name__ == "__main__":
         raise Exception(f"Missing sdist url in response from {pypi_url}")
     if len(sdist_url_dicts) > 1:
         raise Exception(f"Multiple sdist urls in response from {pypi_url}")
-    sdist_file = sdist_url_dicts[0]["filename"]
+    sdist_filename = sdist_url_dicts[0]["filename"]
     sdist_url = sdist_url_dicts[0]["url"]
 
     # Download the sdist
     print(f"Downloading {sdist_url} ... ", end="", flush=True)
-    with requests.get(sdist_url, stream=True) as response:
-        response.raise_for_status()
-        with open(sdist_file, "wb") as f:
-            for chunk in response.iter_content():
-                f.write(chunk)
-    print("Done")
+    with tempfile.TemporaryDirectory() as temp_dir:
+        sdist_filepath = os.path.join(temp_dir, sdist_filename)
+        with requests.get(sdist_url, stream=True) as response:
+            response.raise_for_status()
+            with open(sdist_filepath, "wb") as f:
+                for chunk in response.iter_content():
+                    f.write(chunk)
+        print("Done", flush=True)
 
-    # Generate the commands to run
-    commands = []
-    env_file = os.path.join(folder, "env.sh")
-    if os.path.exists(env_file):
-        commands.append(f". {env_file}")
-    commands.append(f"cibuildwheel --output-dir wheelhouse {sdist_file}")
-    full_command = " && ".join(commands)
+        # Generate the commands to run
+        commands = []
+        env_file = os.path.join(folder, "env.sh")
+        if os.path.exists(env_file):
+            commands.append(f". '{env_file}'")
+        if is_package_pure:
+            tar_temp_dir = Path(tempfile.mkdtemp(dir=temp_dir))
+            with tarfile.open(sdist_filepath) as tar:
+                tar.extractall(path=tar_temp_dir)
 
-    try:
-        print(f"Executing: {full_command}")
-        subprocess.run(full_command, shell=True, check=True)
-    finally:
-        # Clean up
-        os.remove(sdist_file)
+            try:
+                (extracted_sdist_dir,) = tar_temp_dir.iterdir()
+            except ValueError:
+                raise Exception("Invalid sdist: didn't contain a single directory")
+
+            commands.append(f"python3 -m build --wheel --outdir wheelhouse '{extracted_sdist_dir}'")
+        else:
+            commands.append(f"cibuildwheel --output-dir wheelhouse '{sdist_filepath}'")
+        joined_command = " && ".join(commands)
+
+        # Run the commands
+        print(f"Executing: {joined_command}")
+        subprocess.run(joined_command, shell=True, check=True)
