cf3dfa2fe3d0b079b15c9e92e2ac57e7c10c2403
Nicola Soranzo <nicola.soranzo@earlham.ac.uk>
2023-11-30 13:56:52 +0000

    Workaround missing test files in env.sh

--- recipes/pysam/env.sh
+++ recipes/pysam/env.sh
@@ -1,2 +1,5 @@
 export CIBW_BEFORE_ALL_LINUX='yum install -y libcurl-devel zlib-devel bzip2-devel xz-devel && pip install cython'
 export CIBW_SKIP='*-musllinux_* *-macosx_*'
+# Test files are not currently included in the sdist
+export CIBW_TEST_COMMAND=
+export CIBW_TEST_REQUIRES=
