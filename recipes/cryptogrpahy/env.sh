2198352f5e5808fc81574f00eb6318023f246bf4
mvdbeek <m.vandenbeek@gmail.com>
2024-05-29 12:24:58 +0200

    Hack around openssl11 location

--- recipes/cryptogrpahy/env.sh
+++ recipes/cryptogrpahy/env.sh
@@ -1,4 +1,3 @@
 export CIBW_SKIP='*-musllinux_* *-macosx_*'
-export CIBW_BEFORE_ALL_LINUX='yum install -y openssl11 openssl11-devel && curl https://sh.rustup.rs -sSf | sh -s -- -y'
+export CIBW_BEFORE_ALL_LINUX='yum install -y openssl11 openssl11-devel && cp /usr/lib64/pkgconfig/openssl11.pc /usr/lib64/pkgconfig/openssl.pc && curl https://sh.rustup.rs -sSf | sh -s -- -y'
 export CIBW_ENVIRONMENT_LINUX='PATH=$PATH:$HOME/.cargo/bin'
-export CIBW_BEFORE_BUILD_LINUX='export CFLAGS="$CFLAGS $(pkg-config --cflags openssl11)" && export LDFLAGS="$LDFLAGS $(pkg-config --libs openssl11)"'
