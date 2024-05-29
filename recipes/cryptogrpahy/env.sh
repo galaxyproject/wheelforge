bc4ae11967a86ce407e80ad691637f3341ffa36b
mvdbeek <m.vandenbeek@gmail.com>
2024-05-29 12:14:52 +0200

    Export locations before build

--- recipes/cryptogrpahy/env.sh
+++ recipes/cryptogrpahy/env.sh
@@ -1,3 +1,4 @@
 export CIBW_SKIP='*-musllinux_* *-macosx_*'
 export CIBW_BEFORE_ALL_LINUX='yum install -y openssl11 openssl11-devel && curl https://sh.rustup.rs -sSf | sh -s -- -y'
-export CIBW_ENVIRONMENT_LINUX='PATH=$PATH:$HOME/.cargo/bin CFLAGS="$CFLAGS $(pkg-config --cflags openssl11)" LDFLAGS="$LDFLAGS $(pkg-config --libs openssl11)"'
+export CIBW_ENVIRONMENT_LINUX='PATH=$PATH:$HOME/.cargo/bin'
+export CIBW_BEFORE_BUILD_LINUX='export CFLAGS="$CFLAGS $(pkg-config --cflags openssl11)" && export LDFLAGS="$LDFLAGS $(pkg-config --libs openssl11)"'
