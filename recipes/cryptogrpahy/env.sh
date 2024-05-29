47f64f7ec53ea2fc16d7a58c2b062576ae35c82e
mvdbeek <m.vandenbeek@gmail.com>
2024-05-29 11:30:00 +0200

    Append rust path

--- recipes/cryptogrpahy/env.sh
+++ recipes/cryptogrpahy/env.sh
@@ -1,2 +1,2 @@
 export CIBW_BEFORE_ALL_LINUX='yum install -y openssl-devel && curl https://sh.rustup.rs -sSf | sh -s -- -y'
-export CIBW_ENVIRONMENT_LINUX="PATH=$HOME/.cargo/bin:$PATH"
+export CIBW_ENVIRONMENT_LINUX="PATH=$PATH:$HOME/.cargo/bin"
