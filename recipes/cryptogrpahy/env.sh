6b4e96ebe1641b459d0313adfdb5c720e036334a
mvdbeek <m.vandenbeek@gmail.com>
2024-05-29 12:02:09 +0200

    ?

--- recipes/cryptogrpahy/env.sh
+++ recipes/cryptogrpahy/env.sh
@@ -1,3 +1,3 @@
 export CIBW_SKIP='*-musllinux_* *-macosx_*'
-export CIBW_BEFORE_ALL_LINUX='yum install -y openssl11-devel && curl https://sh.rustup.rs -sSf | sh -s -- -y'
+export CIBW_BEFORE_ALL_LINUX='yum install -y openssl11 openssl11-devel && curl https://sh.rustup.rs -sSf | sh -s -- -y'
 export CIBW_ENVIRONMENT_LINUX='PATH=$PATH:$HOME/.cargo/bin'
