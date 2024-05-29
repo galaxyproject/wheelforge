d8d82a74d630ba5dbd928fc10d94d8b08b40a538
mvdbeek <m.vandenbeek@gmail.com>
2024-05-29 12:33:24 +0200

    Skip i686 wheels

--- recipes/cryptogrpahy/env.sh
+++ recipes/cryptogrpahy/env.sh
@@ -1,3 +1,3 @@
-export CIBW_SKIP='*-musllinux_* *-macosx_*'
+export CIBW_SKIP='*-musllinux_* *-macosx_* *_i686'
 export CIBW_BEFORE_ALL_LINUX='yum install -y openssl11 openssl11-devel && cp /usr/lib64/pkgconfig/openssl11.pc /usr/lib64/pkgconfig/openssl.pc && curl https://sh.rustup.rs -sSf | sh -s -- -y'
 export CIBW_ENVIRONMENT_LINUX='PATH=$PATH:$HOME/.cargo/bin'
