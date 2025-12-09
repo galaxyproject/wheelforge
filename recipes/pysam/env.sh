f5a38507aa4d87551f98c4e35b297f67a7c1610b
Nicola Soranzo <nicola.soranzo@earlham.ac.uk>
2025-12-09 14:30:10 +0000

    Build pysam 0.23.3 for Python 3.14

--- recipes/pysam/env.sh
+++ recipes/pysam/env.sh
@@ -1,5 +1,6 @@
 export CIBW_BEFORE_ALL_LINUX='yum install -y libcurl-devel zlib-devel bzip2-devel xz-devel && pip install cython'
-export CIBW_SKIP='*-musllinux_* *-macosx_*'
+export CIBW_BEFORE_ALL_MACOS='brew install -q samtools bcftools'
+export CIBW_BUILD='cp314-*'
 # Test files are not currently included in the sdist
 export CIBW_TEST_COMMAND=
 export CIBW_TEST_REQUIRES=
